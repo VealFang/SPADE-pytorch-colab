f0bc96c5a5c52a03adb88964eed7f2891b43a1cf
Fang <50687763+VealFang@users.noreply.github.com>
2023-06-28 09:01:44 +0800

    Update mvtec.py

--- src/datasets/mvtec.py
+++ src/datasets/mvtec.py
@@ -24,7 +24,8 @@ class MVTecDataset(Dataset):
         self.is_train = is_train
         self.resize = resize
         self.cropsize = cropsize
-        self.mvtec_folder_path = os.path.join(root_path, 'mvtec_anomaly_detection')
+        self.mvtec_folder_path = root_path
+        # self.mvtec_folder_path = os.path.join(root_path, 'mvtec_anomaly_detection')
 
         # download dataset if not exist
         self.download()
